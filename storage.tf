3f46ed7f95c761cace1388f0f8728a0e7f2b940f
Bharath <117035245+bharathcg7@users.noreply.github.com>
2024-11-04 21:20:03 +0000

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "anshistg" {
   name                     = "anshistg"
-  resource_group_name      = azurerm_resource_group.Cloudlearning2.name
-  location                 = azurerm_resource_group.Cloudlearning2.location
+  resource_group_name      = azurerm_resource_group.Cloudlearning1.name
+  location                 = azurerm_resource_group.Cloudlearning1.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
